431241225989dc94e04bcf3b3659f8e518f870a9
VIRSAE\andrew.lin <andrew.lin@virsae.com>
2023-12-04 16:06:34 +1300

    feat: finish all the demo

---
+++ src/app/housing-location.component/housing-location.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housinglocation';
+import {RouterModule} from '@angular/router';
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent implements OnInit{
+  @Input() housingLocation!: HousingLocation;
+
+  ngOnInit() {
+    console.log('ssss');
+  }
+}
